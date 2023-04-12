d36be1b3397d661e49a6805f70b97943a92031db
Ciromontes <ciromontes25@hotmail.com>
2023-04-12 09:14:32 -0500

    agregando dos nuevas propiedades para ese producto, que sería la fecha de registro y la categoría

--- src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
+++ src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
@@ -7,14 +7,12 @@ import javax.persistence.EntityManager;
 import com.latam.alura.tienda.dao.ProductoDao;
 import com.latam.alura.tienda.modelo.Producto;
 import com.latam.alura.tienda.utils.JPAUtils;
+import com.latam.alura.tienda.modelo.Categoria;
 
 public class RegistroDeProducto {
 
 	public static void main(String[] args) {
-		Producto celular=new Producto();
-		celular.setNombre("Samsung");
-		celular.setDescripcion("telefono usado");
-		celular.setPrecio(new BigDecimal("1000"));
+		Producto celular= new Producto("Samsung", "telefono usado", new BigDecimal("1000"),Categoria.CELULARES);
 		
 		
 		EntityManager em = JPAUtils.getEntityManager();
